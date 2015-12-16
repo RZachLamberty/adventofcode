b6be54693fd694fb73900592bd5890f59841af3a
r.zach.lamberty <r.zach.lamberty@gmail.com>
2015-12-15 23:37:43 -0600

    finished day 15

--- day15.py
+++ day15.py
@@ -14,10 +14,10 @@ Usage:
 
 """
 
+import itertools
 import os
+import pandas as pd
 import re
-
-from collections import Counter
 
 
 # ----------------------------- #
@@ -35,50 +35,63 @@ def load_ingredients(fname=FNAME):
     ingredients = {}
     with open(fname, 'r') as f:
         for line in f.readlines():
-            try:
-                i, cap, dur, flav, text, cal = re.match(
-                    r'(\w+): capacity ([-\d]+), durability ([-\d]+), flavor ([-\d]+), texture ([-\d]+), calories ([-\d]+)',
-                    line.strip()
-                ).groups()
-                ingredients[i] = {
-                    'capacity': int(cap),
-                    'durability': int(dur),
-                    'flavor': int(flav),
-                    'texture': int(text),
-                    'calories': int(cal)
-                }
-            except:
-                pass
+            i, cap, dur, flav, text, cal = re.match(
+                r'(\w+): capacity ([-\d]+), durability ([-\d]+), flavor ([-\d]+), texture ([-\d]+), calories ([-\d]+)',
+                line.strip()
+            ).groups()
+            ingredients[i] = {
+                'capacity': int(cap),
+                'durability': int(dur),
+                'flavor': int(flav),
+                'texture': int(text),
+                'calories': int(cal)
+            }
 
     return pd.DataFrame(ingredients)
 
 
-def cookie_score(recipe, ingredients):
-    score = defaultdict(int)
-    for (ing, tsp) in recipe.items():
-        for (qual, val) in ingredients[ing].items():
-            score[qual] += val * tsp
+def cookie_score(recipe, ingredients, calGoal=None):
+    s = ingredients * recipe
+    if calGoal and s.loc['calories'].sum() != calGoal:
+        return 0
+    s = s.drop('calories').sum(axis=1)
+    s[s < 0] = 0
+    return s.prod()
 
 
-def recipes(ings):
-    pass
+def recipes(ingredients):
+    for perm in itertools.product(range(101), repeat=ingredients.shape[1] - 1):
+        l = 100 - sum(perm)
+        if l >= 0:
+            yield perm + (l,)
+
+
+def test_ingredients():
+    return pd.DataFrame({
+        'Butterscotch': {'capacity': -1, 'durability': -2, 'flavor': 6, 'texture': 3, 'calories': 8},
+        'Cinnamon': {'capacity': 2, 'durability': 3, 'flavor': -2, 'texture': -1, 'calories': 3},
+    })
+
+
+def tests():
+    ingredients = test_ingredients()
+    assert cookie_score((44, 56), ingredients) == 62842880
+    assert q_1(ingredients) == 62842880
+    assert cookie_score((40, 60), ingredients, calGoal=500) == 57600000
+    assert q_2(ingredients) == 57600000
 
 
 def q_1(ingredients):
     return max(
         cookie_score(recipe, ingredients)
-        for recipe in recipes(ingredients.keys)
+        for recipe in recipes(ingredients)
     )
 
 
 def q_2(ingredients):
-    return Counter(
-        max(
-            ingredients,
-            key=lambda r: dist_at_t(
-                ingredients[r][0], ingredients[r][1], ingredients[r][2], i
-            )
-        ) for i in range(1, 2504)
+    return max(
+        cookie_score(recipe, ingredients, calGoal=500)
+        for recipe in recipes(ingredients)
     )
 
 
