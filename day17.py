5f8e60a7ee3fa16a360a56ab60a80f59463d16b8
r.zach.lamberty <r.zach.lamberty@gmail.com>
2015-12-16 23:24:12 -0600

    day 17 -- so close, but so far!

---
+++ day17.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Module: day17.py
+Author: zlamberty
+Created: 2015-12-09
+
+Description:
+    day 17 puzzles for the advent of code (adventofcode.com/day/17)
+
+Usage:
+    <usage>
+
+"""
+
+import itertools
+import os
+import pandas as pd
+import re
+
+from collections import defaultdict
+
+
+# ----------------------------- #
+#   Module Constants            #
+# ----------------------------- #
+
+FNAME = os.path.join('data', 'day17.txt')
+
+
+# ----------------------------- #
+#   Main routine                #
+# ----------------------------- #
+
+def load_containers(fname=FNAME):
+    containers = []
+    with open(fname, 'r') as f:
+        return [int(line.strip()) for line in f.readlines()]
+
+
+def vol(containers, combo):
+    return sum(
+        size * num
+        for (size, num) in zip(containers, combo)
+    )
+
+
+def q_1(containers, target=150):
+    return sum(
+        vol(containers, combo) == target
+        for combo in itertools.product(*[range(2) for el in containers])
+    )
+
+
+def q_2(containers, target=150):
+    x = defaultdict(set)
+    for combo in itertools.product(*[range(2) for el in containers]):
+        if vol(containers, combo) == target:
+            x[sum(combo)].add(combo)
+
+    minvol = min(x.keys())
+    return len(x[minvol])
+
+
+# ----------------------------- #
+#   Command line                #
+# ----------------------------- #
+
+if __name__ == '__main__':
+    print "#### day 17 ########################################################"
+    containers = load_containers()
+    print "\tquestion 1 answer: {}".format(q_1(containers))
+    print "\tquestion 2 answer: {}".format(q_2(containers))
